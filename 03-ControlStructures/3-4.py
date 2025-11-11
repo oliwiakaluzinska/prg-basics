e0428950e1fb1ea70dbac58ff4e8967718894345
oliwiakaluzinska <241707@student.uek.krakow.pl>
2025-11-11 12:42:06 +0100

    I did all tasks 3

--- 03-ControlStructures/3-4.py
+++ 03-ControlStructures/3-4.py
@@ -1,7 +1,9 @@
 x = int(input('Enter first number: '))
 y = int(input('Enter second number: '))
 
-if not x < 0 or y < 0 :
+if not x < 0 or not y < 0 :
     print(f'At least one of the numbers {x} and {y} is not negative')
-elif x < 0 and y < 0:
+elif not x > 0 and not y > 0:
     print('Both of the numbers are negative')
+elif x == 0 and y == 0 :
+    print('Both numbers are zero')
