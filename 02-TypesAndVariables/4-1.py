c2bd325a516ddebe402f44cf155ae93b1494caa7
oliwiakaluzinska <241707@student.uek.krakow.pl>
2025-11-24 21:25:02 +0100

    I did tasks from 2-4

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,7 @@
+name = "Adam"
+age = 10
+height = 160
+age_in_6 = 16
+print (f'My name is {name}.')
+print (f'I am {age} years old, and my height is {height} cm.')
+print (f'In 6 years I will be {age_in_6} years old.')
