2dc72e53654b249b859278c565956a271dbdf0ea
Oliwia Kałuzińska <241707@student.uek.krakow.pl>
2026-01-11 13:46:08 +0100

    smth

--- 11-ObjectOrientedProgramming/television.py
+++ 11-ObjectOrientedProgramming/television.py
@@ -2,17 +2,35 @@ class TV:
     def __init__(self):
         self.is_on = False
         self.channel_no = 1
+        self.channels_list = []
+        
     def turn_on(self):
         self.is_on = True
 
     def turn_off(self):
         self.is_on = False
-
-    def change(self, channel_no):
-        self.channel_no = channel_no
-
+    
     def show_status(self):
         if self.is_on:
             return f'TV is on and channel number is: {self.channel_no}'
         else:
             return 'TV is off'
+    
+    def set_channels(self, channels_list):
+        for i in channels_list:
+            self.channels_list.append(i)
+
+    def show_channels(self):
+        print('Channels list:')
+        number = 1
+        if not self.channels_list.empty():
+          for i in self.channels_list:
+            print(f'{number}. {i}')
+            number += 1
+        else:
+            print('Set channels list')
+
+    def change(self, new_channel_no):
+        self.channel_no = new_channel_no
+
+    
--- 11-ObjectOrientedProgramming/tv_show-2.py
+++ 11-ObjectOrientedProgramming/tv_show-2.py
@@ -2,8 +2,13 @@ from television import TV
 
 def main():
     tv = TV()
+    print(tv.show_status())
     tv.turn_on()
-    tv.change(6)
+    print(tv.show_status())
+    print(tv.show_channels())
+    tv.set_channels(['TVP1', 'TVP2', 'Polsat', 'TVN', 'Filmbox', 'Discovery'])
+    print(tv.show_channels())
+    tv.change(5)
     print(tv.show_status())
     tv.turn_off()
     print(tv.show_status())
