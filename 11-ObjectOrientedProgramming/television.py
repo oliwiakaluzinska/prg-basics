0aa51aa477ceb8b69e6886817cb30be81ff2c3a6
Oliwia Kałuzińska <241707@student.uek.krakow.pl>
2026-01-11 13:31:06 +0100

    smth

--- 11-ObjectOrientedProgramming/television.py
+++ 11-ObjectOrientedProgramming/television.py
@@ -1,15 +1,18 @@
 class TV:
     def __init__(self):
         self.is_on = False
-
+        self.channel_no = 1
     def turn_on(self):
         self.is_on = True
 
     def turn_off(self):
         self.is_on = False
 
+    def change(self, channel_no):
+        self.channel_no = channel_no
+
     def show_status(self):
         if self.is_on:
-            return 'TV is on'
+            return f'TV is on and channel number is: {self.channel_no}'
         else:
             return 'TV is off'
--- 11-ObjectOrientedProgramming/tv_show-2.py
+++ 11-ObjectOrientedProgramming/tv_show-2.py
@@ -3,6 +3,7 @@ from television import TV
 def main():
     tv = TV()
     tv.turn_on()
+    tv.change(6)
     print(tv.show_status())
     tv.turn_off()
     print(tv.show_status())
