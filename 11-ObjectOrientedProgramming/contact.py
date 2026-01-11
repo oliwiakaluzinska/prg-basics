7e4061a2d85af3ba9520431283d584cf2d71aa99
Oliwia Kałuzińska <241707@student.uek.krakow.pl>
2026-01-11 15:51:19 +0100

    i modified

--- 11-ObjectOrientedProgramming/contact.py
+++ 11-ObjectOrientedProgramming/contact.py
@@ -3,3 +3,6 @@ class Contact:
         self.name = name
         self.email = email
         self.telephone = telephone
+
+    def __str__(self):
+        return f'{self.name},  {self.email},  {self.telephone}'
--- 11-ObjectOrientedProgramming/contact_list.py
+++ 11-ObjectOrientedProgramming/contact_list.py
@@ -4,8 +4,9 @@ class Contact_list:
     def __init__(self):
         self.arr = []
 
-    def add(self):
-        self.arr.append([Contact])
+    def add(self, name, email, telephone):
+        contact = Contact(name, email, telephone)
+        self.arr.append([contact])
 
     def show(self):
         for i in self.arr:
